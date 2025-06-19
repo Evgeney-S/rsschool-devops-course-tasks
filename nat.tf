0ec612648f624f040a9b45cc8379ee7495f42685
Evgeney Savostyanov <24965432+Evgeney-S@users.noreply.github.com>
2025-06-19 18:09:14 +0400

    Fixed terraform format in bastion.tf nat.tf vpc.tf

--- nat.tf
+++ nat.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "nat" {
-  ami                    = var.ec2_ami
-  instance_type          = var.instance_type
-  subnet_id              = aws_subnet.public[0].id
+  ami                         = var.ec2_ami
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public[0].id
   associate_public_ip_address = true
-  source_dest_check      = false
-  vpc_security_group_ids = [aws_security_group.nat.id]
-  key_name               = var.key_name
+  source_dest_check           = false
+  vpc_security_group_ids      = [aws_security_group.nat.id]
+  key_name                    = var.key_name
   tags = {
     Name              = "nat-instance"
     common-course-tag = var.common_course_tag
